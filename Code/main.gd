c9ede69e052aa5222057c5c6222b50d62f757f2b
Andrés Gutiérrez <andres.gutierrez.arcia@gmail.com>
2018-04-07 16:17:14 -0600

    Cambio de color en las escenas y fallo en el evento del password arreglado

--- Code/main.gd
+++ Code/main.gd
@@ -7,6 +7,7 @@ func _ready():
 
 func _process(delta):
 	$Fish.play()#Hace mover el pez
+	$Carreta.play()
 
 func _on_Salir_pressed():
 	get_tree().quit()#salir del juego
@@ -14,11 +15,8 @@ func _on_Salir_pressed():
 func _on_Iniciar_pressed():
 	get_tree().change_scene("res://Scene/Lessons.tscn")#cambia de escena
 
-
 func _on_LEditUser_text_changed( new_text ):#Guarda el inputtext del username
 	userNameFld = new_text
 
-
-func _on_LEditPass_text_entered( new_text ):#Guarda el inputtext del password
-	userPasswordFld = new_text
-
+func passFld( new_text ):
+	print(new_text)
