6923fdca55c9eeb9e7755564c1bf305ea5e23956
Andrés Gutiérrez <andres.gutierrez.arcia@gmail.com>
2018-05-05 15:08:01 -0600

    Orden de recursos

--- Code/main.gd
+++ Code/main.gd
@@ -6,14 +6,13 @@ func _ready():
 	pass
 
 func _process(delta):
-	$Fish.play()#Hace mover el pez
-	$Carreta.play()
+	pass
 
 func _on_Salir_pressed():
 	get_tree().quit()#salir del juego
 
 func _on_Iniciar_pressed():
-	get_tree().change_scene("res://Scene/Lessons.tscn")#cambia de escena
+	get_tree().change_scene("res://Scene/LessonsScene/Lessons.tscn")#cambia de escena
 
 func _on_LEditUser_text_changed( new_text ):#Guarda el inputtext del username
 	userNameFld = new_text
